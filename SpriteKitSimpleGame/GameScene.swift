50e554b4f60b82ff64b9264939afe5749b238f8b
lovesuper <antononrails@gmail.com>
2018-03-24 13:25:11 +0300

    Finally.

--- SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/GameScene.swift
@@ -1,89 +1,167 @@
-//
-//  GameScene.swift
-//  SpriteKitSimpleGame
-//
-//  Created by Lovesuper on 07/03/2018.
-//  Copyright © 2018 Lovesuper. All rights reserved.
-//
-
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+struct PhysicsCategory {
+  static let None      : UInt32 = 0
+  static let All       : UInt32 = UInt32.max
+  static let Monster   : UInt32 = 0b1
+  static let Projectile: UInt32 = 0b10
+}
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+  func sqrt(a: CGFloat) -> CGFloat {
+    return CGFloat(sqrtf(Float(a)))
+  }
+#endif
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
+  }
+
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+}
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+  let player = SKSpriteNode(imageNamed: "player")
+
+  var monstersDestroyed = 0
+
+  override func didMove(to view: SKView) {
+    backgroundColor = SKColor.white
+    player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+    addChild(player)
+
+    physicsWorld.gravity = CGVector.zero
+    physicsWorld.contactDelegate = self
+
+    run(SKAction.repeatForever(
+      SKAction.sequence([
+        SKAction.run(addMonster),
+        SKAction.wait(forDuration: 1.0)
+        ])
+    ))
+    let backgroundMusic = SKAudioNode(fileNamed: "background-music-aac.caf")
+    backgroundMusic.autoplayLooped = true
+    addChild(backgroundMusic)
+  }
+
+  func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+  }
+
+  func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    return random() * (max - min) + min
+  }
+
+  func addMonster() {
+    let monster = SKSpriteNode(imageNamed: "monster")
+
+    monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
+    monster.physicsBody?.isDynamic = true
+    monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster
+    monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile
+    monster.physicsBody?.collisionBitMask = PhysicsCategory.None
+
+    let actualY = random(min: monster.size.height / 2, max: size.height - monster.size.height / 2)
+
+    monster.position = CGPoint(x: size.width + monster.size.width / 2, y: actualY)
+
+    addChild(monster)
+
+    let actualDuration = random(min: CGFloat(2.0), max: CGFloat(5.0))
+    let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width / 2, y: actualY),
+                                   duration: TimeInterval(actualDuration))
+    let loseAction = SKAction.run() {
+      let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+      let gameOverScene = GameOverScene(size: self.size, won: false)
+      self.view?.presentScene(gameOverScene, transition: reveal)
     }
+
+    monster.run(SKAction.sequence([actionMove, loseAction]))
+  }
+
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    run(SKAction.playSoundFileNamed("pew-pew-lei.caf", waitForCompletion: false))
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    guard let touch = touches.first else {
+      return
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    let touchLocation = touch.location(in: self)
+    let projectile = SKSpriteNode(imageNamed: "projectile")
+
+    projectile.position = player.position
+
+    projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width / 2)
+    projectile.physicsBody?.isDynamic = true
+    projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+    projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+    projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+    projectile.physicsBody?.usesPreciseCollisionDetection = true
+
+    let offset = touchLocation - projectile.position
+    if (offset.x < 0) { return }
+
+    addChild(projectile)
+
+    let direction = offset.normalized()
+    let shootAmount = direction * 1000
+    let realDest = shootAmount + projectile.position
+    let actionMove = SKAction.move(to: realDest, duration: 2.0)
+    let actionMoveDone = SKAction.removeFromParent()
+
+    projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
+  }
+
+  func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
+    projectile.removeFromParent()
+    monster.removeFromParent()
+    monstersDestroyed += 1
+    if (monstersDestroyed >= 10) {
+      let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+      let gameOverScene = GameOverScene(size: self.size, won: true)
+      self.view?.presentScene(gameOverScene, transition: reveal)
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+  }
+
+  func didBegin(_ contact: SKPhysicsContact) {
+    var firstBody: SKPhysicsBody
+    var secondBody: SKPhysicsBody
+    if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+      firstBody = contact.bodyA
+      secondBody = contact.bodyB
+    } else {
+      firstBody = contact.bodyB
+      secondBody = contact.bodyA
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
+    if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
+      (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+      if let monster = firstBody.node as? SKSpriteNode, let
+        projectile = secondBody.node as? SKSpriteNode {
+        projectileDidCollideWithMonster(projectile: projectile, monster: monster)
+      }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
 }
--- SpriteKitSimpleGame/GameViewController.swift
+++ SpriteKitSimpleGame/GameViewController.swift
@@ -1,55 +1,42 @@
-//
-//  GameViewController.swift
-//  SpriteKitSimpleGame
-//
-//  Created by Lovesuper on 07/03/2018.
-//  Copyright © 2018 Lovesuper. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
+    let scene = GameScene(size: view.bounds.size)
+    let skView = view as! SKView
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
+    scene.scaleMode = .resizeFill
 
-    override var prefersStatusBarHidden: Bool {
-        return true
+    skView.presentScene(scene)
+  }
+
+  override var shouldAutorotate: Bool {
+    return true
+  }
+
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    if UIDevice.current.userInterfaceIdiom == .phone {
+      return .allButUpsideDown
+    } else {
+      return .all
     }
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Release any cached data, images, etc that aren't in use.
+  }
+
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
 }
